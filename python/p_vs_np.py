3cc5f31bcf10237f823b9fdbdf39ad96d1fa538a
Juan Lescano <juanpablo.lescano@gmail.com>
2020-02-26 23:05:15 -0300

    one
    credits:
    https://www.youtube.com/watch?v=wf-BqAjZb8M&t=1314s

---
+++ python/p_vs_np.py
@@ -0,0 +1,37 @@
+# XXX -- Top level review comments:
+#
+# * Nice exception recovery and logging.
+#
+# * Please cleanup code formatting.
+#   This is a liggle rough on my eyes.
+#
+# * Should we use this as a template for other
+#   short network element scripts?
+#
+#  -- Thanks.     The Boss :-)
+
+import jnettool.tools.elements.NetworkElement, \
+       jnettool.tools.Routing, \
+       jnettool.tools.RouteInspector
+
+ne=jnnettool.tools.elements.NetworkElement( '171.0.2.45' )
+try:
+       routing_table=ne.getRoutingTable()  # fetch table
+
+except jnettool.tools.elements.MissingVar:
+       # Record table fault
+       logging.exception('''No routing table found''')
+       # Undo partial changes
+       ne.cleanup('''rollback''')
+
+else:
+       num_routes=routing_table.getSize()  # determine table size
+       for RToffset in range( num_routes ):
+              route=routing_table.getRouteByIndex( RToffset )
+              name=route.getName()                 # route name
+              ipaddr=route.getIPAddr()             # ip address
+              print("%15s -> %s" % (name,ipaddr))  # format nicely
+finally:
+       ne.cleanup('''cleanup''') # lockin changes
+       ne.disconnect()
+
