02af21ec4ae163aff3f54c30eb7f6453aafd3d1e
Juan Lescano <juanpablo.lescano@gmail.com>
2020-02-26 23:14:49 -0300

    - indentation must be multiple of 4 - removed spaces in function calls

--- python/p_vs_np.py
+++ python/p_vs_np.py
@@ -5,18 +5,18 @@ import jnettool.tools.RouteInspector
 ne = jnnettool.tools.elements.NetworkElement( '171.0.2.45' )
 
 try:
-       routing_table = ne.getRoutingTable()
+    routing_table = ne.getRoutingTable()
 except jnettool.tools.elements.MissingVar:
-       logging.exception('No routing table found')
-       ne.cleanup('rollback')
+    logging.exception('No routing table found')
+    ne.cleanup('rollback')
 else:
-       num_routes = routing_table.getSize()  # determine table size
-       for RToffset in range( num_routes ):
-              route = routing_table.getRouteByIndex( RToffset )
-              name = route.getName()                 # route name
-              ipaddr = route.getIPAddr()             # ip address
-              print("%15s -> %s" % (name,ipaddr))  # format nicely
+    num_routes = routing_table.getSize()
+    for RToffset in range(num_routes):
+        route = routing_table.getRouteByIndex(RToffset)
+        name = route.getName()
+        ipaddr = route.getIPAddr()
+        print("%15s -> %s" % (name, ipaddr))
 finally:
-       ne.cleanup('cleanup') # lockin changes
-       ne.disconnect()
+    ne.cleanup('cleanup')
+    ne.disconnect()
 
