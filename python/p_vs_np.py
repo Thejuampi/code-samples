5e89727740c487b63a21b7ef1d292092876cdcc0
Juan Lescano <juanpablo.lescano@gmail.com>
2020-02-26 23:11:37 -0300

    - single quotes are easy to type. They used to use double quotes since they came from "C"

--- python/p_vs_np.py
+++ python/p_vs_np.py
@@ -7,8 +7,8 @@ ne = jnnettool.tools.elements.NetworkElement( '171.0.2.45' )
 try:
        routing_table = ne.getRoutingTable()
 except jnettool.tools.elements.MissingVar:
-       logging.exception('''No routing table found''')
-       ne.cleanup('''rollback''')
+       logging.exception('No routing table found')
+       ne.cleanup('rollback')
 else:
        num_routes = routing_table.getSize()  # determine table size
        for RToffset in range( num_routes ):
@@ -17,6 +17,6 @@ else:
               ipaddr = route.getIPAddr()             # ip address
               print("%15s -> %s" % (name,ipaddr))  # format nicely
 finally:
-       ne.cleanup('''cleanup''') # lockin changes
+       ne.cleanup('cleanup') # lockin changes
        ne.disconnect()
 
