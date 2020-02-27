e6e6588f5a7dddf286df023d417f0a576240c8d9
Juan Lescano <juanpablo.lescano@gmail.com>
2020-02-27 00:02:28 -0300

    do not use arrays to represent data!

--- python/misc.py
+++ python/misc.py
@@ -1,9 +1,13 @@
 twitter_search('obama', numtweets=20, retweets=False, unicode=True)
 
-p = (170, 0.1, 0.6)
-if p[1] >= 0.5:
+from collections import namedtuple
+
+Color = namedtuple('Color', ['hue', 'saturation', 'luminosity'])
+
+p = Color(170, 0.1, 0.6)
+if p.saturation >= 0.5:
     print('Whew, that is bright!')
-if p[2] >= 0.5:
+if p.luminosity >= 0.5:
     print('Wow, that is light')
 
 def get_quotes(*args):
