8e1b0a3280b5fbd4a953c90b566db13f97c28d05
Juan Lescano <juanpablo.lescano@gmail.com>
2020-02-26 23:20:44 -0300

    - there must be a better way!

--- python/p_vs_np.py
+++ python/p_vs_np.py
@@ -20,3 +20,10 @@ finally:
     ne.cleanup('commit')
     ne.disconnect()
 
+#################################
+
+from nettools import NetworkElement
+
+with NetworkElement('171.0.2.45') as ne:
+    for route in ne.routing_table:
+        print("%15s -> %s" % (route.name, route.ipaddr))
