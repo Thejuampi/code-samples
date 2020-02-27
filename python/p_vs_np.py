f86f7bb3f01525e96df28adf88acb38e2f865186
Juan Lescano <juanpablo.lescano@gmail.com>
2020-02-26 23:07:48 -0300

    - spaces between '=' operator

--- python/p_vs_np.py
+++ python/p_vs_np.py
@@ -2,9 +2,9 @@ import jnettool.tools.elements.NetworkElement
 import jnettool.tools.Routing
 import jnettool.tools.RouteInspector
 
-ne=jnnettool.tools.elements.NetworkElement( '171.0.2.45' )
+ne = jnnettool.tools.elements.NetworkElement( '171.0.2.45' )
 try:
-       routing_table=ne.getRoutingTable()  # fetch table
+       routing_table = ne.getRoutingTable()  # fetch table
 
 except jnettool.tools.elements.MissingVar:
        # Record table fault
@@ -13,11 +13,11 @@ except jnettool.tools.elements.MissingVar:
        ne.cleanup('''rollback''')
 
 else:
-       num_routes=routing_table.getSize()  # determine table size
+       num_routes = routing_table.getSize()  # determine table size
        for RToffset in range( num_routes ):
-              route=routing_table.getRouteByIndex( RToffset )
-              name=route.getName()                 # route name
-              ipaddr=route.getIPAddr()             # ip address
+              route = routing_table.getRouteByIndex( RToffset )
+              name = route.getName()                 # route name
+              ipaddr = route.getIPAddr()             # ip address
               print("%15s -> %s" % (name,ipaddr))  # format nicely
 finally:
        ne.cleanup('''cleanup''') # lockin changes
