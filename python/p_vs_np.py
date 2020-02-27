c814904c664d853ef1c6382a7e68adb764682ef6
Juan Lescano <juanpablo.lescano@gmail.com>
2020-02-26 23:43:30 -0300

    - more comments about metrics, guidelines, etc

--- python/p_vs_np.py
+++ python/p_vs_np.py
@@ -12,6 +12,16 @@ On the other hand, the code on the bottom is profoundly better
 Code on the top would pass most people's coding standards, it would get checked in
 and would live for a long time and other people would copy that style, and we would congratulate
 we're being "compliant"
+
+What is wrong is "ignoring the Gorilla"
+
+Recommendations:
+* avoid unnecessary packaging in favor of simple imports -> probably solved by using the * import (e.g. import java.lang.*)
+
+* Custom Exceptions -> allows putting a name to the problem
+
+* properties instead of getters -> this is not available in java, but we have @Getter and/or @Accessors(fluent=true)
+ or simply define a routingTable() method
 """
 try:
     routing_table = ne.getRoutingTable()
