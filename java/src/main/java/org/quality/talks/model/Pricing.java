55084b556bded21c4921b6b05ae7690aaf7bdceb
Patricio Bonsembiante <pbonsembiante@gmail.com>
2020-03-11 11:22:48 -0300

    Update Pricing.java

--- java/src/main/java/org/quality/talks/model/Pricing.java
+++ java/src/main/java/org/quality/talks/model/Pricing.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 
 @Getter
 @Builder
 @ToString
 @EqualsAndHashCode
 @RequiredArgsConstructor
+@Accessors(fluent = true)
 public final class Pricing {
   @NonNull private final String symbol;
   @NonNull private final Double price;
